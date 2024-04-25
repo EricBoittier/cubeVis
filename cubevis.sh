e3f8430cac4c774b1eca7d13452167b23585199d
EricBoittier <eric.boittier@icloud.com>
2024-04-25 14:58:21 +0200

    first

---
+++ cubevis.sh
@@ -0,0 +1,28 @@
+#  Read input from command line
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <espfile> <densfile> <scale-bound>"
+    exit 1
+fi
+
+# Assign input parameters to variables
+espfile="s#ESPFILE#${1}#g"
+densfile="s#DENSFILE#${2}#g"
+scalemin="s#SCALEMIN#-${3}#g"
+scalemax="s#SCALEMAX#${3}#g"
+
+echo "ESP file: $espfile"
+echo "Dens file: $densfile"
+echo "Scale min: $scalemin"
+echo "Scale max: $scalemax"
+
+# Format the template file cubevis-template.vmd using sed with alternative delimiter
+sed -e  $espfile \
+-e $densfile \
+-e $scalemin \
+-e $scalemax \
+cubevis-template.vmd > cubevis.vmd
+
+# Run vmd with the generated script
+#vmd  -e cubevis.vmd
+vmd -e cubevis.vmd
+#'/usr/local/lib/vmd/tachyon_LINUXAMD64' -aasamples 2 -res 2000 2000 molecule.dat -o molecule.tga
---
+++ generate.sh
@@ -0,0 +1,15 @@
+bash cubevis.sh data/error1.cube data/1.cube 0.01
+bash cubevis.sh data/21charge1.cube data/1.cube 0.1
+bash cubevis.sh data/esp-1.cube data/1.cube 0.1
+
+bash cubevis.sh data/error2.cube data/2.cube 0.01
+bash cubevis.sh data/21charge2.cube data/2.cube 0.1
+bash cubevis.sh data/esp-2.cube data/2.cube 0.1
+
+bash cubevis.sh data/error3.cube data/3.cube 0.01
+bash cubevis.sh data/21charge3.cube data/3.cube 0.1
+bash cubevis.sh data/esp-3.cube data/3.cube 0.1
+
+bash cubevis.sh data/error4.cube data/4.cube 0.01
+bash cubevis.sh data/21charge4.cube data/4.cube 0.1
+bash cubevis.sh data/esp-4.cube data/4.cube 0.1
